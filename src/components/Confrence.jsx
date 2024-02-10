4032eb6caad45068451eaefd6427f74adcf9180d
alkachhirang <alkachhirang3@gmail.com>
2024-02-10 12:36:16 +0530

    updates

--- src/components/Confrence.jsx
+++ src/components/Confrence.jsx
@@ -10,7 +10,7 @@ const Confrence = () => {
                         <div className='flex max-lg:flex-col items-center gap-[17px] lg:gap-[12px]'>
                             <p className='text-[30px] leading-[45px] text-[#fff] font-bold'>$700</p>
                             <button className='rounded-[50px] hover:shadow-[0_2px_15px_rgba(255,_255,_255,_1)] hover:border-[2px] hover:border-[#fff] hover:text-[#fff] hover:border-solid hover:bg-transparent transition-all ease-linear duration-300 text-[20px] md:text-[24px] border-[#fff] border-[2px] border-solid leading-[30px] md:leading-[36px] font-normal text-[#000] bg-[#fff] px-[31px] lg:px-[39px] py-[10px] lg:py-[15px]'>EXPIRED</button>
-                            <button className='rounded-[50px] hover:shadow-[0_2px_15px_rgba(255,_255,_255,_1)] font-normal text-[20px] md:text-[24px] leading-[30px] md:leading-[36px] text-[#fff] hover:border-solid hover:border-[2px] hover:border-[#fff] hover:bg-transparent bg-[#000] lg:px-[53.5px] lg:py-[15px] py-[10px] px-[85px] transition-all ease-linear duration-300  border-solid border-[2px] border-[#000] text-nowrap'>VIEW SCHEDULE</button>
+                            <button className='rounded-[50px] hover:shadow-[0_2px_15px_rgba(0,_0,_0,_0.5)] font-normal text-[20px] md:text-[24px] leading-[30px] md:leading-[36px] text-[#fff] hover:text-[#000] hover:border-solid hover:border-[2px] hover:border-[#000] hover:bg-transparent bg-[#000] lg:px-[53.5px] lg:py-[15px] py-[10px] px-[85px] transition-all ease-linear duration-300  border-solid border-[2px] border-[#000] text-nowrap'>VIEW SCHEDULE</button>
                         </div>
                     </div>
                 </div>
--- src/components/Sponcer.jsx
+++ src/components/Sponcer.jsx
@@ -9,7 +9,7 @@ const Sponcer = () => {
                         <div className=' flex justify-center items-center flex-col pt-[50px] sm:pt-[80px] md:pt-[118px]'>
                             <p className='md:text-[36px] text-[20px] font-black leading-[30px] md:leading-[54px] text-center md:max-w-[854px] text-[#fff] mb-[24px] lg:mb-[32px] mt-[25px] lg:mt-[0]'>Interested in becoming a sponsor and getting
                                 exposure to our audience?</p>
-                            <button className='rounded-[50px] hover:shadow-[0_2px_15px_rgba(255,_255,_255,_1)] font-normal text-[20px] md:text-[24px] leading-[30px] md:leading-[36px] text-[#fff] bg-[#000] lg:px-[61px] lg:py-[16px] py-[11px] px-[62px] text-nowrap hover:border-[2px] hover:border-solid hover:border-[#fff] hover:bg-transparent border-[2px] border-solid border-[#000] transition-all duration-300 ease-linear'>SPONSOR THE EVENT</button>
+                            <button className='rounded-[50px] hover:shadow-[0_2px_15px_rgba(0,_0,_0,_0.5)] font-normal text-[20px] md:text-[24px] leading-[30px] md:leading-[36px] text-[#fff] hover:text-[#000] bg-[#000] lg:px-[61px] lg:py-[16px] py-[11px] px-[62px] text-nowrap hover:border-[2px] hover:border-solid hover:border-[#000] hover:bg-transparent border-[2px] border-solid border-[#000] transition-all duration-300 ease-linear'>SPONSOR THE EVENT</button>
                         </div>
                     </div>
                 </div>
